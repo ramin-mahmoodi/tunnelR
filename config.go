6302dd07d4e631ebfd937ffae381d1456196a245
ramin-mahmoodi <ramin-mahmoodi@users.noreply.github.com>
2026-02-16 13:07:04 +0330

    Revert FrameSize to 32KB (Fix Regression) -> v2.4.7

--- config.go
+++ config.go
@@ -254,7 +254,7 @@ func applyProfile(c *Config) {
 			c.Smux.KeepAlive = 15
 		}
 		if c.Smux.FrameSize <= 0 {
-			c.Smux.FrameSize = 2048 // 2KB (looks like HTTP/2 frames to DPI)
+			c.Smux.FrameSize = 32768 // Revert to 32KB (2KB caused high-PPS overhead)
 		}
 		// Increase buffers to support ~400Mbps @ 200ms RTT (BDP tuning)
 		if c.Smux.MaxRecv <= 0 || c.Smux.MaxRecv > 2097152 {
