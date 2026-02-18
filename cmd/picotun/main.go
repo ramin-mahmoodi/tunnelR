adbfff1cf78e88c95a00f0475a18ba257d4b3722
ramin-mahmoodi <ramin-mahmoodi@users.noreply.github.com>
2026-02-18 15:19:16 +0330

    chore: bump version to 3.2.0

--- cmd/picotun/main.go
+++ cmd/picotun/main.go
@@ -13,7 +13,7 @@ import (
 	httpmux "github.com/amir6dev/rstunnel/PicoTun"
 )
 
-var version = "3.1.3"
+var version = "3.2.0"
 
 func main() {
 	showVersion := flag.Bool("version", false, "print version and exit")
