ce510a59e2623872f81867f85ab28b1d7aa1d0ef
ramin-mahmoodi <ramin-mahmoodi@users.noreply.github.com>
2026-02-19 01:17:23 +0330

    v3.4.6: Complete UI Redesign & Animations

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@
 # Setup Script (bash <(curl -s https://raw.githubusercontent.com/ramin-mahmoodi/tunnelR/main/setup.sh))
 # ===============================================================
 
-SCRIPT_VERSION="3.4.5"
+SCRIPT_VERSION="3.4.6"
 
 RED='\033[0;31m'
 GREEN='\033[0;32m'
