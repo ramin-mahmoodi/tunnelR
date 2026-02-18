44f8f0987d1f5b0ceedb0f73b88d6b321e39980e
ramin-mahmoodi <ramin-mahmoodi@users.noreply.github.com>
2026-02-18 14:22:51 +0330

    Release v3.1.3: Add Script Version Banner

--- cmd/picotun/main.go
+++ cmd/picotun/main.go
@@ -13,7 +13,7 @@ import (
 	httpmux "github.com/amir6dev/rstunnel/PicoTun"
 )
 
-var version = "3.1.2"
+var version = "3.1.3"
 
 func main() {
 	showVersion := flag.Bool("version", false, "print version and exit")
