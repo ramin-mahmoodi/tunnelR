6282b491208690a60a939d88be74f665a397896f
ramin-mahmoodi <ramin-mahmoodi@users.noreply.github.com>
2026-02-19 16:03:32 +0330

    v3.6.1: Revert Performance Settings (Fix 90Mbps Regression)

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@
 # Setup Script (bash <(curl -s https://raw.githubusercontent.com/ramin-mahmoodi/tunnelR/main/setup.sh))
 # ===============================================================
 
-SCRIPT_VERSION="3.6.0"
+SCRIPT_VERSION="3.6.1"
 
 
 # Colors & Styling
