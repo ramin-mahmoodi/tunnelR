6e08ad8a595ad2d77a69b22a8dc10057498185e5
ramin-mahmoodi <ramin-mahmoodi@users.noreply.github.com>
2026-02-19 17:56:56 +0330

    v3.6.4: Revert Forced Connection Pool (Back to 8)

--- cmd/picotun/main.go
+++ cmd/picotun/main.go
@@ -13,7 +13,7 @@ import (
 	httpmux "github.com/amir6dev/rstunnel/PicoTun"
 )
 
-var version = "3.6.3"
+var version = "3.6.4"
 
 func main() {
 	showVersion := flag.Bool("version", false, "print version and exit")
